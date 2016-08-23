4cd16d123dff20af2d30d55a835887a7cdf65bdc
Romulo <lds@dev.home>
2016-08-23 16:44:59 -0300

    start da implementação !

--- astar.c
+++ astar.c
@@ -6,56 +6,126 @@ struct node{
     struct node* next;
     int g;
     int h;
+    int f;
+    int x, y;
     float weight;
 };
 
 struct node* head_open = NULL;
+struct node* head_close = NULL;
 
 void insert(struct node** head, struct node * node);
 void removeNode(struct node** head, struct node * node);
+int containsNode(struct node** head, struct node * node);
+
+struct node init_node(int x, int y, float weight){
+    struct node no;
+    no.x = x;
+    no.y = y;
+    no.weight = weight;
+}
+
+struct node* lowest_f(){
+   struct node* current = head_open;
+   struct node* lowest_f = current;
+   while(current != NULL){
+       if(current->f < lowest_f->f){
+           lowest_f = current;
+       }
+       current = current->next;
+   }
+   return lowest_f;
+}
+
+int is_finish(struct node node, int x, int y){
+    if(node.x == x && node.y == y){
+        return 1;
+    }
+    return 0;
+}
+
+int heuristic(struct node* node, struct node* finish){
+    return abs(node->x - finish->x) + abs(node->y - finish->y);
+}
+
+void g(struct node current, struct node neighbor){
+
+    int dist = 0;
+    if(current.x == neighbor.x || current.y == neighbor.y ){
+        dist=10;
+    }else{
+        dist=14;
+    }
+    neighbor.g = current.g + dist;
+        
+    
+}
 
 
 int main(){
 
+    int x0=1, y0=1;//start 
+    int xf=3, yf=3;//finish
 
-    struct node* n = (struct node*)malloc(sizeof(struct node));
-    n->g=10;
-    struct node* n2= (struct node*)malloc(sizeof(struct node));
-    n2->g=20;
-    struct node* n3= (struct node*)malloc(sizeof(struct node));
-    n3->g=30;
-    insert(&head_open, n);
-    insert(&head_open, n2);
-    insert(&head_open, n3);
+    struct node grid[4][4];
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            grid[i][j] = init_node(i, j, 0);
+        }
+    }
 
-    struct node* aux = head_open;
-    while(aux->next != NULL){
-        printf("%d\n", aux->g);
-        aux = aux->next;
-    }	
-    printf("%d\n", aux->g);
-    removeNode(&head_open, n3);
-    removeNode(&head_open, n);
+    
+    struct node start_node = grid[1][1];
+    start_node.g=0;
+    start_node.h=0;
+    start_node.f = start_node.g + start_node.h;
 
-    struct node* auxr= head_open;
-    while(auxr->next != NULL){
-        printf("%d\n", auxr->g);
-        auxr = auxr->next;
-    }	
-    printf("%d\n", auxr->g);
+    //insere o start point na lista de open
+    insert(&head_open, &start_node);
+
+    while(head_open != NULL){
+        struct node* current = lowest_f();
+        if(is_finish(*current, xf, yf)){
+            //TODO rebuild the path
+            printf("finish!");
+            return 0;
+        }
+
+        removeNode(&head_open, current);
+        insert(&head_close, current);
+
+        //Checando a vizinhança
+        //checar em caso de borda
+        for(int x = current->x-1; x <= current->x+1; x++){
+            for(int y = current->x-1; y <= current->x+1; y++){
+                struct node neighbor = grid[x][y];
+                //avoid reinsert the current node at open list
+                if(x == current->x && y== current->y){
+                    continue;
+                }
+                if(containsNode(&head_close, current)){
+                    continue;
+                }
+
+                g(*current, neighbor);
+                neighbor.h=heuristic(&neighbor, &grid[3][3]);
+                printf("[%d][%d] - G=%d \n H=%d \n F=%d\n ", x,y,neighbor.g, neighbor.h, neighbor.f);
 
 
+            }
+        }
+    }
 }
 
 void insert(struct node** head, struct node* inode){	
     if(*head == NULL){
         *head = inode;	
     }else{
-        struct node* aux = *head;
-        while(aux->next != NULL){
-            aux = aux->next;
+        struct node* current = *head;
+        while(current->next != NULL){
+            current = current->next;
         }	
-        aux->next = inode;
+        current->next = inode;
     }
 
 }
@@ -67,14 +137,14 @@ void removeNode(struct node** head, struct node* inode){
             //lista 1 elemento
             *head = NULL;
         }else{
-            struct node* aux = *head;
-            if(aux == inode){ //remove a head
-                *head = aux->next;
+            struct node* current = *head;
+            if(current == inode){ //remove a head
+                *head = current->next;
             }else{
-                while(inode != aux->next){
-                    aux = aux->next;
+                while(inode != current->next){
+                    current = current->next;
                 }
-                aux->next=aux->next->next;
+                current->next=current->next->next;
             }
 
 
@@ -82,3 +152,23 @@ void removeNode(struct node** head, struct node* inode){
 
     }
 }
+
+int containsNode(struct node** head, struct node* node){	
+    if(*head == NULL){
+        return 0; 
+    }
+
+    struct node* current = *head;
+    do{
+        if(current == node){
+            return 1;
+        }
+        current = current->next;
+    }
+    while(current != NULL);
+    
+
+    return 0;
+
+}
+
